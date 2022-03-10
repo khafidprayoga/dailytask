54633900ff9010436c7f36127675d0263d965076
Khafid Prayoga <khafidp@pm.me>
2022-03-10 10:22:12 +0700

    registering exceptions middleware

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { json } = require('body-parser');
 const app = express();
+const serverRouter = require('./routes');
+const exceptionsMiddleware = require('./middlewares/exceptions');
 
 app.disable('x-powered-by');
 app.use(json());
-
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello World!' });
-});
+app.use(serverRouter);
+app.use(exceptionsMiddleware);
 
 module.exports = app;
