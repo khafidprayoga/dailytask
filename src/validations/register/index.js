1b95cc110ffd4081846641f69c501b3d9d0ec3e6
Khafid Prayoga <khafidp@pm.me>
2022-03-10 11:25:31 +0700

    implement backtick config options for schema validation

--- src/validations/register/index.js
+++ src/validations/register/index.js
@@ -1,10 +1,11 @@
 const { postSchema } = require('./schema');
+const { options } = require('../options');
 const ValidationError = require('../../exceptions/ValidationError');
 
 const RegisterValidations = {
   async postValidate(payload) {
     try {
-      await postSchema.validateAsync(payload);
+      await postSchema.validateAsync(payload, options);
     } catch (error) {
       throw new ValidationError(error.details[0].message);
     }
