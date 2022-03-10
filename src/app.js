846ce86bbf4941c3157448de3d2f6d3750f822b7
Khafid Prayoga <khafidp@pm.me>
2022-03-10 15:16:47 +0700

    mount middleware for 404 not found service request

--- src/app.js
+++ src/app.js
@@ -11,4 +11,9 @@ app.use(serverRouter); // mount all registered routes
 // If ClientError and ServerError push the error to exceptions middleware
 app.use(exceptionsMiddleware);
 
+// if route not found, and exceptions not thrownm handle with 404 middleware
+app.use((req, res, next) => {
+  res.status(404).send({ status: 'failed', message: 'Endpoint not found!' });
+});
+
 module.exports = app;
