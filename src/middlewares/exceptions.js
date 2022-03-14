4b6489a85e9021b6698cab4db73bcf8919934494
Khafid Prayoga <khafidp@pm.me>
2022-03-14 14:17:13 +0700

    Refactoring: handle client error from 400 to 417
    After 417 the HTTP error status is unused and unassigned
    see: https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml

--- src/middlewares/exceptions.js
+++ src/middlewares/exceptions.js
@@ -1,7 +1,7 @@
 const exceptionsMiddleware = (error, req, res, next) => {
   const { statusCode, message } = error;
 
-  statusCode === 400
+  statusCode >= 400 || statusCode <= 417
     ? res.status(error.statusCode).send({ status: 'failed', message })
     : res
         .status(500)
